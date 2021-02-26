443849d96810a4eb5f1c1466741d69affa5d1349
frank <frank.chen@skylinetw.com>
2021-02-26 15:34:01 +0800

    [feat] add style
    - page style
    - global style

---
+++ src/styles/styled.js
@@ -0,0 +1,46 @@
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const StyledContainer = styled.div`
+  padding: 50px;
+`;
+
+export const StyledTitle = styled.h1`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+export const StyledIcon = styled.img`
+  width: 50px;
+  margin-right: 10px;
+`;
+
+export const StyledCard = styled.div`
+  border: 1px solid #ccc;
+  padding: 20px;
+  margin-bottom: 20px;
+`;
+
+export const StyledLink = styled.a`
+  word-break: break-all;
+`;
+
+export const StyleLoadingObserver = styled.div`
+  margin: 15px auto;
+  border: 5px solid #f3f3f3;
+  border-top: 5px solid #e6a35e;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  animation: ${spin} 2s linear infinite;
+`;
