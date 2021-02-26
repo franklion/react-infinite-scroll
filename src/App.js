82f74ce7fdbb2d1b86cb6cafca1a0776bc9cb104
frank <frank.chen@skylinetw.com>
2021-02-26 17:51:34 +0800

    [feat] extract IntersectionObserver
    - useIntersection

--- src/App.js
+++ src/App.js
@@ -1,50 +1,30 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useAsync from './hooks/useAsync';
 import useSkipFirstMount from './hooks/useSkipFirstMount';
+import useIntersection from './hooks/useIntersection';
 import { fetchReposAsync } from './api/repo';
 import iconLion from './static/lion.png';
 import cons from './constant';
-import {
-  StyledContainer,
-  StyledTitle,
-  StyledIcon,
-  StyledCard,
-  StyledLink,
-  StyleLoadingObserver
-} from './styles/styled';
+import { StyledContainer, StyledTitle, StyledIcon, StyledCard, StyledLink, StyledTarget } from './styles/styled';
 
 const App = () => {
-  const refObserver = useRef(null);
-  const refContainer = useRef(null);
-  const refLoadingObserver = useRef(null);
   const [params, setParams] = useState(cons.INIT_PARAMS);
   const [repos, setRepos] = useState([]);
-
-  const { isLoading, execute, response, error } = useAsync(fetchReposAsync, {
+  const { execute, response, error } = useAsync(fetchReposAsync, {
     immediate: true,
     initParams: params
   });
-
-  useEffect(() => {
-    const options = {
-      root: refContainer.current,
-      rootMargin: '0px 0px 200px 0px',
-      threshold: 0
-    };
-
-    const callback = ([entry]) => {
-      if (entry && entry.isIntersecting) {
-        setParams((prevParams) => ({ ...prevParams, pageNum: prevParams.pageNum + 1 }));
-      }
-    };
-
-    refObserver.current = new IntersectionObserver(callback, options);
-    refObserver.current.observe(refLoadingObserver.current);
-  }, []);
+  const [refObserver, refContainer, refTarget] = useIntersection({
+    update: function () {
+      setParams((prevParams) => ({ ...prevParams, pageNum: prevParams.pageNum + 1 }));
+    }
+  });
 
   useSkipFirstMount(() => {
     if (params) {
-      execute(params);
+      setTimeout(() => {
+        execute(params);
+      }, 1000);
     }
   }, [params]);
 
@@ -52,10 +32,18 @@ const App = () => {
     if (response && response?.data.length > 0) {
       setRepos((prevRepos) => [...prevRepos, ...response?.data]);
     } else if (response && response?.data.length === 0) {
-      refObserver.current.unobserve(refLoadingObserver.current);
-      refLoadingObserver.current.remove();
+      refObserver.current.disconnect();
+      refTarget.current.remove();
     }
-  }, [response]);
+  }, [response, refObserver, refTarget]);
+
+  useEffect(() => {
+    if (error) {
+      console.log('API 請求到達上限，請稍候再試！');
+      refObserver.current.disconnect();
+      refTarget.current.remove();
+    }
+  }, [error, refObserver, refTarget]);
 
   return (
     <StyledContainer rel={refContainer}>
@@ -63,6 +51,7 @@ const App = () => {
         <StyledIcon src={iconLion} alt="lion" />
         {cons.INIT_PARAMS.user} repo list:
       </StyledTitle>
+
       {repos.map((repo) => (
         <StyledCard key={repo.id}>
           <h2>{repo.name}</h2>
@@ -72,7 +61,8 @@ const App = () => {
           </StyledLink>
         </StyledCard>
       ))}
-      <StyleLoadingObserver ref={refLoadingObserver} />
+
+      <StyledTarget ref={refTarget} />
     </StyledContainer>
   );
 };
---
+++ src/hooks/useIntersection.js
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from 'react';
+
+const useIntersection = ({ update }) => {
+  const refContainer = useRef(null);
+  const refTarget = useRef(null);
+  const refObserver = useRef(
+    new window.IntersectionObserver(
+      ([entry]) => {
+        if (entry && entry.isIntersecting) {
+          console.log('update');
+          update();
+        }
+      },
+      {
+        root: refContainer.current,
+        rootMargin: '0px 0px 100px 0px',
+        threshold: 0
+      }
+    )
+  );
+
+  useEffect(() => {
+    const { current: currentObserver } = refObserver;
+    currentObserver.disconnect();
+
+    if (refTarget.current) currentObserver.observe(refTarget.current);
+
+    return () => currentObserver.disconnect();
+  }, []);
+
+  return [refObserver, refContainer, refTarget];
+};
+
+export default useIntersection;
