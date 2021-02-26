835e9d1395563b21656222ab04f66a6a54448b05
frank <frank.chen@skylinetw.com>
2021-02-26 15:35:10 +0800

    [feat] add api repo
    - to request repo API

---
+++ src/api/repo.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+/**
+ * @typedef {Object} LoginResponse
+ * @property {number} code - 請求回應代碼
+ * @property {Object} [data] - 所有資訊集合
+ * @property {string} [msg] - 錯誤訊息
+ */
+
+/**
+ * 取得個人所有的 repo
+ * @returns {Promise<FetchResponse>}
+ */
+export function fetchReposAsync({ user, pageSize, pageNum }) {
+  return axios({
+    method: 'GET',
+    url: `https://api.github.com/users/${user}/repos`,
+    params: {
+      per_page: pageSize,
+      page: pageNum
+    }
+  });
+}
